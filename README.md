e947e1f713387855efa8135b60c83dec7965d503
Les Carbonaro <lcarbonaro@gmail.com>
2020-12-20 18:57:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# py-sel-api
+Build API from Python Selenium example.
+
+- [Step notes](https://bit.ly/34N83Zt)
+
+
+Related reference links:
+
+- [Flask](https://flask.palletsprojects.com/en/1.1.x/)
+- [Json](https://docs.python.org/3/library/json.html)
+- [Requests](https://requests.readthedocs.io/en/master/)
+- [Jinja](https://jinja.palletsprojects.com/en/2.11.x/)
+
+
+-----
+
+Join us at Mississauga Coding! 
+=================================
+
+Find out about upcoming events by checking out: 
+----------------------------------------------
+
+- [Our Meetup page](http://www.meetup.com/Mississauga-Coding/) 
+- [Our Slack channel](https://bit.ly/2uEnt2C)
+
+Post your questions on any of these forums, attend our events, learn and share.
+
+Happy Coding!
+
+
+![Mississauga Coding](https://github.com/MississaugaCoding/mississaugacoding.github.io/blob/master/favicon-32x32.png "Mississauga Coding")
+
+
+
+
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+
+from selenium import webdriver
+
+import json
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'] )   # default is GET only
+def index():
+    if request.method == 'GET':        
+        return render_template("index.html", hotels=[])
+    else:    
+        # POST
+        loc = request.form['location']
+        response = requests.get(url="http://127.0.0.1:5000/api/" + loc)
+        results = response.json()
+        return render_template("index.html", hotels=results)
+
+
+@app.route('/api/<location>')
+def scrape_booking(location):
+    # start Chrome session
+    browser = webdriver.Chrome()
+
+    # opening web site
+    url = 'https://www.booking.com/'
+    browser.get(url)
+
+    # type in location
+    search_input_el = browser.find_element_by_name('ss')
+    search_input_el.send_keys(location)
+
+    # click search button
+    search_btn_el = browser.find_element_by_css_selector('button.sb-searchbox__button')
+    search_btn_el.click()  
+
+    # read results
+    list = []
+    result_els = browser.find_elements_by_css_selector('div.sr_item')
+    for result in result_els:    
+        name_el = result.find_element_by_class_name('sr-hotel__name')
+        obj = {}
+        obj['name'] = name_el.text
+        list.append(obj)
+        
+    # close Chrome session
+    browser.quit()
+
+    return json.dumps(list)
